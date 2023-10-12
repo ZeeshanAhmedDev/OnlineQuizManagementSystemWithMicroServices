5da25af2c798bb741aaab2663068be32b6377f57
ZeeshanAhmedDev <shanisukk@gmail.com>
2023-10-12 12:34:13 +0200

    Added Microservices and extended project

---
+++ Questions-Microservice/target/classes/data.sql
@@ -0,0 +1,109 @@
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
+values(
+	1, 
+	'What does HTML stand for?',
+	'Home Text Markup Langauge',
+	'Hyper Text Markup Langauge',
+	'HyperLink Text Markup Langauge',
+	3,
+	-1)
+;
+
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
+values(
+	2,
+	'Which tag is used for inserting the largest heading in HTML?',
+	'head',
+	'<h1>',
+	'</h1>',
+	2,
+	-1)
+;
+
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
+values(
+	3,
+	'Which is used to create Web Pages ?',
+	'C++',
+	'HTML',
+	'JVM',
+	2,
+	-1)
+;
+
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
+values(
+	4,
+	'HTML is a set of markup ___.',
+	'tags',
+	'sets',
+	'attributes',
+	1,
+	-1)
+;
+
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
+values(
+	5,
+	'HTML program is saved using ___ extension.',
+	'htmll',
+	'.htnl',
+	'.html',
+	3,
+	-1)
+;
+
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
+values(
+	6,
+	'How many sizes of headers are available in HTML by default?',
+	'5',
+	'6',
+	'4',
+	2,
+	-1)
+;
+
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
+values(
+	7,
+	'How to create an ordered list in HTML?',
+	'<ul>',
+	'<ol>',
+	'<b>',
+	2,
+	-1)
+;
+
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
+values(
+	8,
+	'We enclose HTML tags within?',
+	'{}',
+	'!!',
+	'<>',
+	3,
+	-1)
+;
+
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
+values(
+	9,
+	'What is the effect of the <b> tag?',
+	'It make the font black',
+	'It converts the text within it to bold font',
+	'None',
+	2,
+	-1)
+;
+
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
+values(
+	10,
+	'Which of the following tags doesn’t require a closing tag?',
+	'<br>',
+	'<hr>',
+	'Both A and B',
+	3,
+	-1)
+;
